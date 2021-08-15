56e8eb1e61d601c13aa4d952160948287ab256e0
Saqib <saqib29111@gmail.com>
2021-08-15 14:29:53 +0600

    Navbar add

---
+++ flutter_webpage/lib/Navbar/Navbar.dart
@@ -0,0 +1,58 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+
+class Navbar extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints){
+        if(constraints.maxWidth > 1200){
+          return DesktopNavbar();
+        } else if(constraints.maxWidth>800 && constraints.maxWidth<1200){
+          return DesktopNavbar();
+        } else {
+          return MobileNavbar();
+        }
+      },
+      
+    );
+  }
+}
+
+class DesktopNavbar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Text("Saqib Studio", style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30,)
+          ),
+
+          Row(
+            children: <Widget>[
+              Text("Home", style: TextStyle(color: Colors.white)),
+              Text("About us", style: TextStyle(color: Colors.white)),
+              Text("Portfolio", style: TextStyle(color: Colors.white)),
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MobileNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- flutter_webpage/lib/main.dart
+++ flutter_webpage/lib/main.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webpage/Navbar/Navbar.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
@@ -42,7 +35,9 @@ class MyHomePage extends StatelessWidget {
           )
         ),
         child: Column(
-          
+          children: <Widget>[
+            Navbar(),
+          ],
         ),
       ),
     ); 
